5d72ab6db9e1f22b6f468f9ab29aca8fc8e01627
Fjnnn2907 <phuongphipho2005@gmail.com>
2024-06-21 08:53:09 +0700

    fix Camera

--- Assets/Fields/Scripts/FollowedCamera.cs
+++ Assets/Fields/Scripts/FollowedCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,21 @@ using UnityEngine;
 public class FollowedCamera : MonoBehaviour
 {
     public float FollowSpeed;
-    public float Height;
     public Transform target;
     public Vector2 Max;
     public Vector2 Min;
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (transform.position != target.position)
+        if(this.transform.position != target.position)
         {
-            Vector3 newPos = new Vector3(target.position.x,target.position.y + Height, -10f);
+            Vector3 targetPositon = new Vector3(target.position.x, target.position.y, -10);
 
-            newPos.x = Mathf.Clamp(target.position.x,Min.x,Max.x);
-            newPos.y = Mathf.Clamp(target.position.y,Min.y,Max.y);
+            targetPositon.x = Mathf.Clamp(targetPositon.x,Min.x,Max.x);
 
-            transform.position = Vector3.Lerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
+            targetPositon.y = Mathf.Clamp(targetPositon.y,Min.y,Max.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPositon, FollowSpeed);
         }
     }    
 }
